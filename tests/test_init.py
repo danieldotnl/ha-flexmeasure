bc767a9159832d19de0ac1066a025d737badd366
Daniel <danieldotnl@outlook.com>
2022-06-05 20:55:34 +0000

    Fix tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -5,11 +5,11 @@ from custom_components.flexmeasure.const import DOMAIN
 from custom_components.flexmeasure.const import DOMAIN_DATA
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from .const import MOCK_CONFIG
+from .const import MOCK_TIME_CONFIG_FINAL
 
 
 async def test_reload_entry(hass):
-    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="1234")
+    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_TIME_CONFIG_FINAL, entry_id="1234")
     entry.add_to_hass(hass)
     await hass.config_entries.async_setup(entry.entry_id)
     assert DOMAIN_DATA in hass.data and entry.entry_id in hass.data[DOMAIN_DATA]
